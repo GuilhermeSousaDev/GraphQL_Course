5de07bb4a71f7194a2d58ab04a4a66695594f1de
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2023-04-19 17:36:24 -0300

    Schemas Type

--- example-01/index.js
+++ example-01/index.js
@@ -1,36 +1,67 @@
 const { gql, ApolloServer } = require('apollo-server');
 
+const products = [
+    {
+        id: 1,
+        name: 'Notebook',
+        price: 5000.00,
+    },
+    {
+        id: 2,
+        name: 'TV',
+        price: 10000.00,
+    },
+    {
+        id: 3,
+        name: 'Tablet',
+        price: 3000.00,
+    }
+]
+
+const users = [
+    {
+        id: 1,
+        name: 'Bruno',
+        money: 1500.00,
+        active: true,
+        age: 18,
+    },
+    {
+        id: 2,
+        name: 'Cleber',
+        money: 2000.00,
+        active: true,
+        age: 21,
+    }
+]
+
 const typeDefs = gql`
-    type Query {
+    type Products {
+        id: ID,
+        name: String,
+        price: Float,
+    }
+    type Users {
         age: Int,
         money: Float,
         name: String,
         active: Boolean,
         id: ID,
-        tecnologies: [String!]!,
+    }
+    type Query {
+        users: [Users],
+        products: [Products],
     }
 `;
 
 const resolvers = {
     Query: {
-        age() {
-            return 18;
+        users() {
+            return users;
         },
-        money() {
-            return 1580.90;
-        },
-        name() {
-            return 'Walter';
-        },
-        active() {
-            return true;
-        },
-        id() {
-            return 12345235;
-        },
-        tecnologies() {
-            return ['GraphQl', 'React', 'Javascript', 'Css'];
-        },
+        products() {
+            return products;
+        }
     }
 }
 
