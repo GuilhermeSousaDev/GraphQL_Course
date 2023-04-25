d7caaa9eb90887f619506c41162e95f2cad37218
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2023-04-25 09:08:14 -0300

    Aliases GraphQl

--- example-02/index.js
+++ example-02/index.js
@@ -1,34 +1,35 @@
 const { gql, ApolloServer } = require('apollo-server');
 
-const db = [
-    {
-        id: 1,
-        name: 'Paulo',
-        email: 'paulo@gmail.com',
-        fixPhoneNumber: 77999887766,
-        profile: 1,
-    },
-    {
-        id: 2,
-        name: 'Diego',
-        email: 'diego@gmail.com',
-        fixPhoneNumber: 77999554433,
-        profile: 2,
-    },
-    {
-        id: 3,
-        name: 'Iago',
-        email: 'iago@gmail.com',
-        fixPhoneNumber: 77999776655,
-        profile: 3,
-    }
-];
-
-const profiles = [
-    { id: 1, description: 'ADMIN' },
-    { id: 2, description: 'NORMAL' },
-    { id: 3, description: 'EMPLOYEER' },
-]
+const db = {
+    users: [
+        {
+            id: 1,
+            name: 'Paulo',
+            email: 'paulo@gmail.com',
+            phoneNumber: 77999887766,
+            profile: 1,
+        },
+        {
+            id: 2,
+            name: 'Diego',
+            email: 'diego@gmail.com',
+            phoneNumber: 77999554433,
+            profile: 2,
+        },
+        {
+            id: 3,
+            name: 'Iago',
+            email: 'iago@gmail.com',
+            phoneNumber: 77999776655,
+            profile: 3,
+        },
+    ],
+    profiles: [
+        { id: 1, description: 'ADMIN' },
+        { id: 2, description: 'NORMAL' },
+        { id: 3, description: 'EMPLOYEER' },
+    ]
+};
 
 const typeDefs = gql`
     type User {
@@ -45,25 +46,18 @@ const typeDefs = gql`
     type Query {
         user(id: Int): User,
         profiles: [Profile],
+        users: [User]
     }
 `;
 
 const resolvers = {
     User: {
-        phoneNumber(obj) {
-            return obj.fixPhoneNumber;
-        },
-        profile(obj) {
-            return profiles.find(profile => profile.id === obj.id);
-        }
+        profile: (obj) => db.profiles.find(profile => profile.id === obj.id),
     },
     Query: {
-        user(_, args) {
-            return db.find(user => user.id === args.id);
-        },
-        profiles() {
-            return profiles;
-        }
+        user: (_, args) => db.users.find(user => user.id === args.id),
+        profiles: () => db.profiles,
+        users: () => db.users,
     }
 }
 
