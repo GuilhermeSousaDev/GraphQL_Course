e86ab6af805ef28de682ad42a2cd5d14329956bb
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2023-04-29 09:18:52 -0300

    GraphQL Mutations

--- example-02/src/graphql/modules/User/resolvers.js
+++ example-02/src/graphql/modules/User/resolvers.js
@@ -1,5 +1,9 @@
 const db = require('../../../db');
 
+function generateId(list) {
+    return list[list.length - 1]?.id + 1 || 1;
+}
+
 module.exports = {
     User: {
         profile: (obj) => db.profiles.find(profile => profile.id === obj.id),
@@ -7,5 +11,18 @@ module.exports = {
     Query: {
         user: (_, args) => db.users.find(user => user.id === args.id),
         users: () => db.users,
+    },
+    Mutation: {
+        createUser(_, args) {
+            const newUser = {
+                ...args,
+                id: generateId(db.users),
+                profile_id: 2
+            }
+
+            db.users.push(newUser);
+
+            return newUser;
+        }
     }
 }
